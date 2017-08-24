3e2711d76a44f2cb3c6e1ea550476543540e11ca
Scott Mandrell <sdmandrell@berkeley.edu>
2017-08-23 21:02:24 -0700

    made client server with text boxes

--- QtMessengerDemo/mainwindow.cpp
+++ QtMessengerDemo/mainwindow.cpp
@@ -9,6 +9,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    receive_data_over_network();
 }
 
 MainWindow::~MainWindow()
@@ -19,7 +21,6 @@ MainWindow::~MainWindow()
 void MainWindow::on_button1_clicked()
 {
     qDebug() << "Pushed button1"; // printing to console
-
 }
 
 void MainWindow::on_submitData_clicked()
@@ -28,4 +29,23 @@ void MainWindow::on_submitData_clicked()
     QByteArray arr = data.toLocal8Bit();
     char* processedData = arr.data();
     qDebug() << processedData;
+    QUdpSocket socket2;
+    socket2.writeDatagram(arr, QHostAddress("127.0.0.1"), 8088);
+}
+
+void MainWindow::receive_data_over_network()
+{
+    socket = new QUdpSocket(this);
+    socket->bind(8088);
+    connect(socket, SIGNAL(readyRead()), this, SLOT(on_receive()));
+}
+
+void MainWindow::on_receive()
+{
+    while (socket->hasPendingDatagrams()) {
+        QByteArray datagram;
+        datagram.resize(socket->pendingDatagramSize());
+        socket->readDatagram(datagram.data(), datagram.size());
+        ui->receivedData->append(datagram.data());
+    }
 }
--- QtMessengerDemo/mainwindow.h
+++ QtMessengerDemo/mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 #include <QTextEdit>
+#include <QTcpSocket>
+#include <QUdpSocket>
 
 namespace Ui {
 class MainWindow;
@@ -15,13 +17,18 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void receive_data_over_network();
+
 private slots:
     void on_button1_clicked();
     void on_submitData_clicked();
+    void on_receive();
 
 private:
     Ui::MainWindow *ui;
     QTextEdit *data;
+    QTextEdit *receivedData;
+    QUdpSocket *socket;
 };
 
 #endif // MAINWINDOW_H
