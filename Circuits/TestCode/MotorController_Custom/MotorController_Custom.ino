bf9abf7ac1c0febbbf09fa08b107eec01f4557d0
Mitchell Oleson <moleson21@gmail.com>
2017-10-31 17:26:21 -0700

    Fixed motor controller test code - works as expected now

--- Circuits/TestCode/MotorController_Custom/MotorController_Custom.ino
+++ Circuits/TestCode/MotorController_Custom/MotorController_Custom.ino
@@ -1,19 +1,47 @@
 
-int dirPin = 1;
-int potPin = 1;
-int maxDuty = 1023;
+int dirPinIN = 2;
+int dirPinOUT = 3;
+int PWMPinOUT = 6;
+int potPinIN = A1;
+
+int dirOLD = 0;
+int dirNEW = 0;
 int dutyCycle = 0;
 
-
 void setup() {
-  pinMode(dirPin, OUTPUT);
+  pinMode(dirPinIN, INPUT);
+  pinMode(dirPinOUT, OUTPUT);
+  pinMode(PWMPinOUT, OUTPUT);
+
+  analogWrite(PWMPinOUT, 0);
+  delay(100);
+  
+  dirNEW = digitalRead(dirPinIN);
+  digitalWrite(dirPinOUT, dirNEW);
+  dirOLD = dirNEW;
+
+  Serial.begin(9600);
 }
 
 void loop() {
+  dirNEW = digitalRead(dirPinIN);
+  if (dirNEW != dirOLD)
+  {
+    Serial.println("Switching Direction!");
+
+    analogWrite(PWMPinOUT, 0);
+    delay(100);
+
+    digitalWrite(dirPinOUT, dirNEW);
+    dirOLD = dirNEW;
+  }
 
   // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
-  dutyCycle = analogRead(potPin);
-  analogWrite(dirPin, dutyCycle / 4)
+  dutyCycle = min(255, analogRead(potPinIN) / 2);
+  analogWrite(PWMPinOUT, dutyCycle);
+  
+  Serial.print("Speed: ");
+  Serial.println(dutyCycle);
 
   // Wait short period
   delay(100);
